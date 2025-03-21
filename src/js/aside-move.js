d2ad028e2a1459b140e831d72c144f0eb81b3364
Ritin-git <ritingitproject@gmail.com>
2025-03-21 15:51:16 +0530

    Update aside-move.js

--- src/js/aside-move.js
+++ src/js/aside-move.js
@@ -1,3 +1,9 @@
-jQuery(document).ready(function ($) {
-    $(".aside").insertAfter(".card-horizontal.blog-post-item.post-1");
+document.addEventListener("DOMContentLoaded", function () {
+    const blogContainer = document.querySelector(".blog");
+    const aside = document.querySelector(".aside");
+    const firstPost = document.querySelector(".card-horizontal.blog-post-item.post-1");
+
+    if (blogContainer && aside && firstPost) {
+        blogContainer.insertBefore(aside, firstPost.nextSibling);
+    }
 });
